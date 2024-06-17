60479a62bec547706ae209e2710a57a0cecbb81e
Dif <zzeni42@gmail.com>
2024-06-18 00:52:45 +0300

    Jinja template tags

--- trydjango/pages/views.py
+++ trydjango/pages/views.py
@@ -8,7 +8,15 @@ def home_view(request, *args, **kwargs):
     return render(request, 'home.html', {})
 
 def contact_view(request, *args, **kwargs):
+    my_context = {}
     return render(request, 'contact.html', {})
 
 def about_view(request, *args, **kwargs):
-    return render(request, 'about.html', {})
+    my_context = {
+        'title': 'This is about us',
+        'this is true': True,
+        'my_number': 123,
+        'my_list': [553, 7665, 5656],
+        'my_html': '<h1>Hello, World</h1>'
+    }
+    return render(request, 'about.html', my_context)
