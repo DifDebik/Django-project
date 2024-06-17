6b2f313111b26fce17b476372065088e3f36c6d3
Dif <zzeni42@gmail.com>
2024-06-18 02:23:52 +0300

    Pure forms

--- trydjango/products/views.py
+++ trydjango/products/views.py
@@ -5,16 +5,29 @@ from .forms import ProductForm
 # Create your views here.
 
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
-
+    #print(request.GET)
+    #print(request.POST)
+    if request.method == 'POST':
+        new_title = request.POST.get('title')
+        #Product.objects.create(title=new_title)
+        print(new_title)
     context = {
-        'form': form,
+        
     }
 
     return render(request, 'products/product_create.html', context)
+
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
+
+#     context = {
+#         'form': form,
+#     }
+
+#     return render(request, 'products/product_create.html', context)
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
