550602ff1341bc65299644ff32edb3b888bdcbf8
Dif <zzeni42@gmail.com>
2024-07-15 22:43:45 +0300

    form validation methods

--- trydjango/products/forms.py
+++ trydjango/products/forms.py
@@ -1,7 +1,13 @@
 from django import forms
+
 from .models import Product
 
+
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(label='',
+                            widget=forms.TextInput(attrs={'placeholder': "Your Title"}))
+    email = forms.EmailField()
+
     class Meta:
         model = Product
         fields = [
@@ -10,7 +16,28 @@ class ProductForm(forms.ModelForm):
             'price',
         ]
 
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get('title')
+        if "CFE" not in title:
+            raise forms.ValidationError("This is not a valid title")
+        return title
+
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get('email')
+        if not email.endswith('edu'):
+            raise forms.ValidationError("This is not a valid email")
+        return email
+
+
 class RawProductForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
-    price = forms.DecimalField()
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': "Your Title"}))
+    description = forms.CharField(required=False,
+                                  widget=forms.Textarea(
+                                      attrs={
+                                          'placeholder': "Your Description",
+                                          'class': 'new-class-name two',
+                                          'id': 'my-id-for-textarea',
+                                          'rows': 100,
+                                          'cols': 120,
+                                      }))
+    price = forms.DecimalField(initial=199.99)
--- trydjango/products/views.py
+++ trydjango/products/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render
+
+from .forms import ProductForm
 from .models import Product
-from .forms import ProductForm, RawProductForm
+
 
 # Create your views here.
 
-def product_create_view(request):
-    my_form = RawProductForm()
-    if request.method == 'POST':
-        my_form = RawProductForm(request.POST)
-        if my_form.is_valid():
-            #the data is good
-            print(my_form.cleaned_data)
-            Product.objects.create(**my_form.cleaned_data)
-        else:
-            print(my_form.errors)
-    context = {
-        'form': my_form
-    }
 
-    return render(request, 'products/product_create.html', context)
+# def product_create_view(request):
+#     my_form = RawProductForm()
+#     if request.method == 'POST':
+#         my_form = RawProductForm(request.POST)
+#         if my_form.is_valid():
+#             # the data is good
+#             print(my_form.cleaned_data)
+#             Product.objects.create(**my_form.cleaned_data)
+#         else:
+#             print(my_form.errors)
+#     context = {
+#         'form': my_form,
+#     }
+
+#     return render(request, 'products/product_create.html', context)
 
 
 # def product_create_view(request):
@@ -29,22 +32,22 @@ def product_create_view(request):
 #         #Product.objects.create(title=new_title)
 #         print(new_title)
 #     context = {
-        
 #     }
 
 #     return render(request, 'products/product_create.html', context)
 
-# def product_create_view(request):
-#     form = ProductForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form = ProductForm()
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
 
-#     context = {
-#         'form': form,
-#     }
+    context = {
+        'form': form,
+    }
 
-#     return render(request, 'products/product_create.html', context)
+    return render(request, 'products/product_create.html', context)
+
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
@@ -53,6 +56,6 @@ def product_detail_view(request):
     #     'description': obj.description,
     # }
     context = {
-        'object': obj
+        'object': obj,
     }
     return render(request, 'products/product_detail.html', context)
